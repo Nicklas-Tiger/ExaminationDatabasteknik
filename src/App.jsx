56a9ac318918146a0ff65dee9b248a2474102ad1
Nicklas Tiger <nicklas.tiger@gmail.com>
2025-02-17 11:00:33 +0100

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Projects from "./pages/Projects";
+import CreateProject from "./pages/CreateProject";
+import StartPage from "./pages/StartPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/create" element={<CreateProject />} />
+          <Route path="/" element={<StartPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
